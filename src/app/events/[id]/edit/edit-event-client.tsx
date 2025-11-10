d0857fcfa776904dd2d8376e3ce4c14abe4b407b
Geo-Bold <edgeorge.archbold@gmail.com>
2025-11-10 17:41:40 -0500

    refactor: remove EditEventClient component as it is no longer needed

--- src/app/events/[id]/edit/edit-event-client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { EventForm } from '../../components/event-form';
-import type { EditEventClientProps } from '../../types';
-
-export function EditEventClient({ event }: EditEventClientProps) {
-  return <EventForm event={event} mode="edit" />;
-}
-
