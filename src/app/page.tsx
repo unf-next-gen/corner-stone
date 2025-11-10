db197a6bfcb0eab16664c45bfbb980681f7dc62d
notevenangela <angelalian0205@gmail.com>
2025-11-10 18:42:36 -0500

    fix: update link to profile page and format import statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Link from "next/link";
-import { Button } from '@mantine/core';
+import { Button } from "@mantine/core";
 
 export default function Home() {
   return (
     <div>
       <h1>This is a test</h1>
-      <Link href="/cornerstone/account">
+      <Link href="/profile">
         <button style={{ padding: "10px", marginTop: "10px" }}>
           Go to Cornerstone Account
         </button>
