0ee077635a842b758a68326a6f105267b078808e
Geo <edgeorge.archbold@gmail.com>
2025-12-04 21:55:36 -0500

    feat(navbar): add navigation items to AppConfig with icons for Home, Events, Volunteers, and Settings

--- src/global/config/config.ts
+++ src/global/config/config.ts
@@ -1,4 +1,12 @@
+import { IconCalendarStats, IconHome2, IconSettings, IconUsers } from "@tabler/icons-react";
+
 export const AppConfig = {
+  navbar: [
+      { icon: IconHome2, label: "Home", href: "/" },
+      { icon: IconCalendarStats, label: "Events", href: "/events" },
+      { icon: IconUsers, label: "Volunteers", href: "/volunteers" },
+      { icon: IconSettings, label: "Settings", href: "/settings" },
+    ],
 } as const;
 
 
