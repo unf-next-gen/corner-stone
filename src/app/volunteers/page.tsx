530037bc1fdf5ac4f6da6e852066736cbcea1308
Geo <edgeorge.archbold@gmail.com>
2025-12-05 22:04:53 -0500

    style(volunteers): refactor layout to include Container component and improve spacing for better presentation

--- src/app/volunteers/page.tsx
+++ src/app/volunteers/page.tsx
@@ -2,33 +2,34 @@
 
 import { GetVolunteers } from "./actions";
 import { getUser } from "../auth/actions";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 import VolunteerManager from "./components/VolunteerManager";
-import { Stack, Paper, Title, Divider } from "@mantine/core";
+import { Stack, Paper, Title, Divider, Container } from "@mantine/core";
 
 export default async function Volunteers() {
-
-
-
   const volunteers = await GetVolunteers();
 
   if (!volunteers) {
-    return (<div className="p-8 text-center">
-      <h1 className="text-2xl font-bold text-red-600">Nothing to show here</h1>
-
-    </div>);
+    return (
+      <div className="p-8 text-center">
+        <h1 className="text-2xl font-bold text-red-600">
+          Nothing to show here
+        </h1>
+      </div>
+    );
   }
 
-
   return (
-    <Stack gap="sm" h="100%">
-      <Paper withBorder h="100%">
-        <Stack>
-          <Title order={1}>Volunteer Directory</Title>
-          <Divider />
-          <VolunteerManager data={volunteers} />
-        </Stack>
-      </Paper>
-    </Stack>
+    <Container size="xl" py="xl">
+      <Stack gap="sm" h="100%">
+        <Paper withBorder h="100%">
+          <Stack p={"lg"}>
+            <Title order={1} p={"lg"}>Volunteer Directory</Title>
+            <Divider />
+            <VolunteerManager data={volunteers} />
+          </Stack>
+        </Paper>
+      </Stack>
+    </Container>
   );
 }
