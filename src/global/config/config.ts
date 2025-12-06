84fe186766dae2a2736992307d2c85f019f347d3
Geo <edgeorge.archbold@gmail.com>
2025-12-05 22:05:44 -0500

    feat(navbar): add 'Register' item to navbar with clipboard icon for improved navigation

--- src/global/config/config.ts
+++ src/global/config/config.ts
@@ -1,10 +1,12 @@
-import { IconCalendarStats, IconHome2, IconSettings, IconUsers } from "@tabler/icons-react";
+import { IconCalendarStats, IconClipboardList, IconHome2, IconSettings, IconUsers } from "@tabler/icons-react";
+
 
 export const AppConfig = {
   navbar: [
       { icon: IconHome2, label: "Home", href: "/" },
       { icon: IconCalendarStats, label: "Events", href: "/events" },
       { icon: IconUsers, label: "Volunteers", href: "/volunteers" },
+      { icon: IconClipboardList, label: "Register", href: "/registration" }, 
       { icon: IconSettings, label: "Settings", href: "/settings" },
     ],
 } as const;
